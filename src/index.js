0963100ab836e452495f5543a4e3a7d8511c760b
a12989x <alexis12989x@gmail.com>
2020-11-02 13:16:27 -0600

    Import normalize styles and main.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import 'normalize.css';
+import './styles/main.scss';
+
 import App from './App';
 
 render(<App />, document.getElementById('root'));
