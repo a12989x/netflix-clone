989da5092be1f8761d7322169d6a0c3970ded197
a12989x <alexis12989x@gmail.com>
2020-11-02 17:41:47 -0600

    Set up Jumbotron component

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const Jumbotron = () => {
+const Jumbotron = ({ id, title, subTitle, img, alt }) => {
     return (
-        <div>
-            <p>Jumbotron Component</p>
-        </div>
+        <section className='jumbotron'>
+            <div className='jumbotron__container'>
+                <h1 className='jumbotron__title'>{title}</h1>
+                <h2 className='jumbotron__subTitle'>{subTitle}</h2>
+                <img
+                    className={`jumbotron__img ${
+                        id === 2 ? 'jumbotron__img-reverse' : ''
+                    }`}
+                    src={img}
+                    alt={alt}
+                />
+            </div>
+        </section>
     );
 };
 
