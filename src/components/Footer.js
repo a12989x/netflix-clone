784ac7f388b7e8bcae6af27493040996490ac451
a12989x <alexis12989x@gmail.com>
2020-11-03 11:25:30 -0600

    Set up Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,99 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer Component</p>
-        </div>
+        <section className='footer'>
+            <p className='footer__title'>Questions? Contact us.</p>
+
+            <ul className='footer__list'>
+                <li>
+                    <a className='footer__link' href='#'>
+                        FAQ
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Help Center
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Account
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Media Center
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Investor Relations
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Jobs
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Redeem Gift Cards
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Buy Gift Cards
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Ways to Watch
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Terms of Use
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Privacy
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Cookie Preferences
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Coporate Information
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Contact Us{' '}
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Speed Test
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Legal Notices
+                    </a>
+                </li>
+                <li>
+                    <a className='footer__link' href='#'>
+                        Netflix Originals
+                    </a>
+                </li>
+            </ul>
+
+            <p className='footer__country'>Netflix US</p>
+        </section>
     );
 };
 
