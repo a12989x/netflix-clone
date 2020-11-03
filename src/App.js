30bd6ce746e346fbf7a3724e1d17e02dc01da9df
a12989x <alexis12989x@gmail.com>
2020-11-03 11:28:02 -0600

    Import Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Home from './components/pages/Home';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div className='app'>
             <Home />
+            <Footer />
         </div>
     );
 };
