33dc965b420652922c83701e2ac5705ede7c9688
a12989x <alexis12989x@gmail.com>
2020-11-04 16:32:37 -0600

    Create config for routes and create home route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import Home from './components/pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div className='app'>
-            <Home />
-            <Footer />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                </Switch>
+                <Footer />
+            </BrowserRouter>
         </div>
     );
 };
