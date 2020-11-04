5c9c5204c1c9c07e3e2a3e6ce6c6de1579e72650
a12989x <alexis12989x@gmail.com>
2020-11-04 10:46:15 -0600

    Set up Faq component

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
-const Faq = () => {
+const Faq = ({ id, header, body, answerOpen, setAnswerOpen }) => {
+    const handleClick = () => {
+        if (answerOpen === id) setAnswerOpen(null);
+        else setAnswerOpen(id);
+    };
+
     return (
-        <div>
-            <p>Faq Component</p>
-        </div>
+        <li className='faq'>
+            <button className='faq__btn' onClick={handleClick}>
+                {header}
+                <svg
+                    id='thin-x'
+                    viewBox='0 0 19 19'
+                    className={`faq__svg-icon ${
+                        answerOpen === id ? 'faq__svg-icon-open' : ''
+                    }`}
+                    focusable='true'
+                >
+                    <path d='M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z'></path>
+                </svg>
+            </button>
+            <p
+                className={`faq__answer ${
+                    answerOpen === id ? 'faq__answer-open' : ''
+                }`}
+            >
+                <span>{body}</span>
+            </p>
+        </li>
     );
 };
 
