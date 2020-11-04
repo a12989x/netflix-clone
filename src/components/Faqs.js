88e61085c8e09e6f6e4a5bbc0947067dcd4d9e6b
a12989x <alexis12989x@gmail.com>
2020-11-04 16:21:51 -0600

    Set up Faqs component

--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import faqsData from '../fixtures/faqs.json';
+
+import Faq from './Faq';
+import CtaForm from './CtaForm';
 
 const Faqs = () => {
+    const [answerOpen, setAnswerOpen] = useState(null);
+
     return (
-        <div>
-            <p>Faqs Component</p>
-        </div>
+        <section className='faqs'>
+            <h3 className='faqs__title'>Frequently Asked Questions</h3>
+
+            <ul>
+                {faqsData.map((item) => (
+                    <Faq
+                        key={item.id}
+                        {...item}
+                        setAnswerOpen={setAnswerOpen}
+                        answerOpen={answerOpen}
+                    />
+                ))}
+            </ul>
+
+            <CtaForm />
+        </section>
     );
 };
 
