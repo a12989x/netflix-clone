c8d36e780bf745ff6fc69a8e79aef6043ec47d14
a12989x <alexis12989x@gmail.com>
2020-11-12 11:05:22 -0600

    Import Header component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import jumboData from '../../fixtures/jumbo.json';
-import Faqs from '../Faqs';
 
+import Header from '../Header';
 import Jumbotron from '../Jumbotron';
+import Faqs from '../Faqs';
 
 const Home = () => {
     return (
         <div className='home'>
+            <Header />
             {jumboData.map((item) => (
                 <Jumbotron key={item.id} {...item} />
             ))}
