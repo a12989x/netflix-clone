37ec79e0b6d0621471c037a430e884098c2c1bef
a12989x <alexis12989x@gmail.com>
2020-11-12 11:05:03 -0600

    Set up Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import HomeBg from '../img/misc/home-bg.jpg';
+import { ReactComponent as Logo } from '../img/misc/logo.svg';
+
+import CtaForm from './CtaForm';
 
 const Header = () => {
     return (
-        <div>
-            <p>Header Component</p>
-        </div>
+        <header className='header'>
+            <img className='header__imgBg' src={HomeBg} alt='home background' />
+            <nav className='header__nav'>
+                <Logo className='header__logo' />{' '}
+                <Link to='/sign-in'>
+                    <button>Sign In</button>
+                </Link>
+            </nav>
+
+            <section className='header__main'>
+                <h1 className='header__title'>
+                    Unlimited movies, TV shows, and more.
+                </h1>
+                <h2 className='header__subtitle'>
+                    Watch anywhere. Cancel anytime.
+                </h2>
+                <CtaForm />
+            </section>
+        </header>
     );
 };
 
