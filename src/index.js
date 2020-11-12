c6dbf61cbc247db7793b651522bfdcfff1f84c58
a12989x <alexis12989x@gmail.com>
2020-11-12 14:25:52 -0600

    Import FirebaseContextProvider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import FirebaseContextProvider from './components/contexts/FirebaseContext';
+
 import 'normalize.css';
 import './styles/main.scss';
 
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+render(
+    <FirebaseContextProvider>
+        <App />
+    </FirebaseContextProvider>,
+    document.getElementById('root')
+);
