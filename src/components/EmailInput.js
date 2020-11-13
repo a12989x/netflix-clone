86fe170a424c3a7a030c0a074ec472e4695b0103
a12989x <alexis12989x@gmail.com>
2020-11-13 15:56:32 -0600

    Create EmailInput Component

---
+++ src/components/EmailInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmailInput = () => {
+    return (
+        <div>
+            <p>InputEmail Component</p>
+        </div>
+    );
+};
+
+export default EmailInput;
