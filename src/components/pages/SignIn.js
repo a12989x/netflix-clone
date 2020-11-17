ed3d4ce75444b94c6e6ffd264f03d3ba9f02e870
a12989x <alexis12989x@gmail.com>
2020-11-17 13:16:16 -0600

    Set up SignIn component

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import HomeBg from '../../img/misc/home-bg.jpg';
+import { ReactComponent as Logo } from '../../img/misc/logo.svg';
+
+import SignInForm from '../SignInForm';
 
 const SignIn = () => {
     return (
-        <div>
-            <p>SignIn Component</p>
+        <div className='signIn'>
+            <img className='signIn__imgBg' src={HomeBg} alt='home background' />
+            <Logo className='signIn__logo' />
+
+            <SignInForm />
         </div>
     );
 };
