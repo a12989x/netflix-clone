0f69d017168eb032b1cd49a4f1d2949d9d34d37b
Alexis Guzman <alexis12989x@gmail.com>
2020-11-19 13:35:02 -0600

    Import and add AuthContextProvider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import FirebaseContextProvider from './components/contexts/FirebaseContext';
+import AuthContextProvider from './components/contexts/AuthContext';
 
 import 'normalize.css';
 import './styles/main.scss';
@@ -10,7 +11,9 @@ import App from './App';
 
 render(
     <FirebaseContextProvider>
-        <App />
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
     </FirebaseContextProvider>,
     document.getElementById('root')
 );
