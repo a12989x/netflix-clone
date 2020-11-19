9ef88a44c5f0918f85ef0ab08b411ceb80c90704
Alexis Guzman <alexis12989x@gmail.com>
2020-11-19 13:29:07 -0600

    Create SignUpForm component

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpForm = () => {
+    return (
+        <div>
+            <p>SignUpForm component</p>
+        </div>
+    );
+};
+
+export default SignUpForm;
