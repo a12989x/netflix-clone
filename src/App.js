99b8df636a89389f3b4800cbc02cc9c7c3aba21c
Alexis Guzman <alexis12989x@gmail.com>
2020-11-21 13:21:08 -0600

    Set up for Redirect Route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Home from './components/pages/Home';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import { AuthContext } from './components/contexts/AuthContext';
+import { RedirectRoute } from './components/helpers/routes';
 
 import Footer from './components/Footer';
 import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
+import Browse from './components/pages/Browse';
 
 const App = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <div className='app'>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/signin' component={SignIn} />
-                    <Route exact path='/signup' component={SignUp} />
+                    <RedirectRoute
+                        user={user}
+                        exact
+                        path={'/'}
+                        signPath={'/browse'}
+                    >
+                        <Home />
+                    </RedirectRoute>
+
+                    <RedirectRoute
+                        user={user}
+                        exact
+                        path={'/signin'}
+                        signPath={'/browse'}
+                    >
+                        <SignIn />
+                    </RedirectRoute>
+
+                    <RedirectRoute
+                        user={user}
+                        exact
+                        path={'/signup'}
+                        signPath={'/browse'}
+                    >
+                        <SignUp />
+                    </RedirectRoute>
+
+                    <RedirectRoute
+                        user={!user}
+                        exact
+                        path={'/browse'}
+                        signPath={'/signin'}
+                    >
+                        <Browse />
+                    </RedirectRoute>
                 </Switch>
                 <Footer />
             </BrowserRouter>
