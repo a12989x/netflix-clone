34da8ba66af93897c35b6c6235a93e907ed54f07
Alexis Guzman <alexis12989x@gmail.com>
2020-11-21 13:20:02 -0600

    Create SignUp function and create useEffect for cookies

--- src/components/contexts/AuthContext.js
+++ src/components/contexts/AuthContext.js
@@ -1,5 +1,6 @@
-import React, { useContext, createContext, useState } from 'react';
+import React, { useState, useContext, createContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import cookie from 'react-cookies';
 
 import { FirebaseContext } from './FirebaseContext';
 
@@ -9,12 +10,33 @@ export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
     const history = useHistory();
+
     const { firebase } = useContext(FirebaseContext);
-    const [values, handleChange] = useForm({ email: '', password: '' });
+    const [user, setUser] = useState(cookie.load('user'));
+    const [values, handleChange] = useForm({
+        name: '',
+        email: '',
+        password: '',
+    });
     const [error, setError] = useState('');
+
+    useEffect(() => {
+        const listener = firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+                cookie.save('user', authUser, { path: '/', sameSite: true });
+                setUser(authUser);
+            } else {
+                cookie.remove('authUser');
+                setUser(null);
+            }
+        });
+
+        return () => listener();
+    }, []);
 
     const SignIn = (e) => {
         e.preventDefault();
+        setError('');
 
         firebase
             .auth()
@@ -23,8 +45,28 @@ const AuthContextProvider = ({ children }) => {
             .catch((err) => setError(err.message));
     };
 
+    const SignUp = (e) => {
+        e.preventDefault();
+        setError('');
+
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(values.email, values.password)
+            .then((result) =>
+                result.user
+                    .updateProfile({
+                        displayName: values.name,
+                        photoURL: Math.floor(Math.random() * 5) + 1,
+                    })
+                    .then(() => history.push('/'))
+            )
+            .catch((err) => setError(err.message));
+    };
+
     return (
-        <AuthContext.Provider value={{ values, handleChange, error, SignIn }}>
+        <AuthContext.Provider
+            value={{ values, handleChange, user, error, SignIn, SignUp }}
+        >
             {children}
         </AuthContext.Provider>
     );
